5c69b40d6e95f7f8274e886105698e9bfc94f798
Alterous Lucifer <lualterous@gmail.com>
2023-05-17 17:25:40 -0700

    Add Project Files

---
+++ Assets/Coding/Universal Machine/EnscribedDisc.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements.Experimental;
+
+namespace UniversalMachine
+{
+    public class EnscribedDisc : MonoBehaviour
+    {
+        public Transform Disc;
+
+
+        public float Diameter;
+
+        public string Enscription;
+
+        public double RotationRate;
+
+        public float Height;
+
+        public float Rotation;
+
+        public Vector3 Flow;
+
+        public CurvedText Text;
+
+        public Func<List<Particle>> Quanta;
+
+        System.Random r = new System.Random();
+
+
+        void Start()
+        {
+            Text.text = Enscription;
+            Flow = Disc.up + Disc.right;
+        }
+
+        public int Number()
+        {
+            char[] enscr = Enscription.ToCharArray();
+
+            int i = 0;
+            foreach (char c in enscr)
+            {
+                i += c - 65;
+            }
+
+            return i;
+        }
+
+        public float Energy()
+        {
+            return Number() / Height * Diameter;
+        }
+
+        public Vector3 Offset()
+        {
+            return Flow * Rotation * Diameter;
+        }
+
+        void Update()
+        {
+            Disc.localScale = new Vector3((float)Diameter * 1.45f, (float)Height * 0.03f, (float)Diameter * 1.45f);
+            Disc.localPosition = new Vector3(Disc.localPosition.x, (float)Height, Disc.localPosition.z);
+
+            Disc.Rotate(new Vector3(0, 1, 0), (float)RotationRate);
+
+            Rotation = Disc.localRotation.y;
+        }
+
+        void ApplyForce(Particle particle)
+        {
+            double energy = r.NextDouble() * Energy() / UniversalMachine.Particle.EnergeticResistance;
+            Vector3 offset = Offset();
+
+            //offset = new Vector3(offset.x * transform.up.x, offset.y * transform.up.y, offset.z * transform.up.z);
+
+            particle.AddForce(transform.up * (float)energy, offset, Time.deltaTime);
+        }
+
+        void FixedUpdate()
+        {
+            foreach(Particle unit in Quanta())
+            {
+                ApplyForce(unit);
+            }
+        }
+    }
+}
---
+++ Assets/Coding/Universal Machine/PathMarker.cs
@@ -0,0 +1,92 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Misc;
+
+namespace UniversalMachine
+{
+    public class PathMarker : MonoBehaviour
+    {
+        public class Mark : ScriptableObject
+        {
+            public Vector3 Position;
+            public Vector3 Direction;
+            public float Length;
+            public Vector3 Energy;
+
+            public Vector3 Proceed(Vector3 position, Vector3 destination, Vector3 energy)
+            {
+                Vector3 a = destination - position;
+
+                Vector3 x = a.normalized;
+                Vector3 y = Direction;
+
+                float angle = Vector3.Angle(x, y);
+
+                float attack = 1 / 180 * angle;
+
+                float distance = (Position + (Direction * (Length / 2))).magnitude
+                    - (position + (x * (a.magnitude / 2))).magnitude;
+
+                Vector3 warpDirection = new Vector3(y.x, y.y, y.z);
+                Vector3 warpEnergy = Energy / Length;
+                Vector3 pathEnergy = energy * a.magnitude;
+
+                //Vector3 warp = new Vector3((warpEnergy.x - pathEnergy.x) * warpDirection.x,
+                //    (warpEnergy.y - pathEnergy.y) * warpDirection.y,
+                //    (warpEnergy.z - pathEnergy.z) * warpDirection.z);
+
+                Vector3 warp = warpDirection *
+                    (warpEnergy.magnitude / attack - pathEnergy.magnitude * attack) /
+                    distance;
+
+                return warp;
+            }
+        }
+
+        public KeyedList<Mark> Path = new KeyedList<Mark>();
+
+        public Dictionary<int,Mark> Striations = new Dictionary<int,Mark>();
+
+        public Vector3 Project(Vector3 start, Vector3 end, Vector3 energy)
+        {
+            Vector3 final = Vector3.zero;
+
+            foreach (Mark marker in Path)
+            {
+                final += marker.Proceed(start, final, energy);
+                Debug.Log("Proceed: " + marker.Proceed(start, final, energy));
+            }
+
+            Move(start, end, energy);
+
+            return (final + end) / 2;
+        }
+
+        public void Move(Vector3 start, Vector3 end, Vector3 energy)
+        {
+            Mark path = ScriptableObject.CreateInstance<Mark>();
+
+            path.Position = start;
+            path.Direction = (end - start).normalized;
+            path.Length = (end - start).magnitude;
+            path.Energy = energy;
+
+            Path.Add(path);
+
+            Striations.Add(Striations.Count, path);
+        }
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
